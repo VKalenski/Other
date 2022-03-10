7d35a6b614b27587428341b89eb9f2f460739d7e
VKalenski <vilislav.kalenski@elando.com>
2022-03-10 14:13:53 +0200

    Files

--- JS_Advanced/JS Advanced - Exams/Exam Prep 2 - 15.02.2021/02/02. Christmas Dinner.js
+++ JS_Advanced/JS Advanced - Exams/Exam Prep 2 - 15.02.2021/02/02. Christmas Dinner.js
@@ -1,33 +1,41 @@
 class ChristmasDinner {
+    // Създаваме списък с продукти
     constructor(budget) {
-        this.budget = budget;
-        this.products = [];
-        this.dishes = [];
-        this.guests = {};
+        this.budget = budget; // this.budget автоматично извиква set-ъра
+        this.products = []; // продуктът е масив, като винаги ще има две свойства - име и цена
+        this.dishes = []; // празен масив
+        this.guests = {}; // празен обект
     }
 
+    // Валидацията става с get and set
+    // Свойстовото budget всъщност е get-ър и set-ър
     get budget() {
-        return this._budget;
+        return this._budget; // _budget - наименование на private property, като важното е, да не е budget, защото иначе влизаме в рекурсия
     }
+    // _budget пази стойността и когато тя ни потрябва я взимаме от get-ъра
+    // _budget не се инициализира директно в конструктора, защото по този начин заобикаляме валидатора
 
     set budget(value) {
         if (value < 0) {
-            throw new Error("The budget cannot be a negative number");
+            throw new Error("The budget cannot be a negative number"); // Хвърляме грешка при по-малко от 0
         }
-        return this._budget = value;
+        return this._budget = value; // _budget - наименование на private property, 
     }
 
+    // Method
     shopping(product) {
         let [item, price] = product;
         if (this._budget < price) {
             throw new Error("Not enough money to buy this product");
         }
 
-        this.products.push(item);
+        // При преминаване на проверката следва добавянето в масива
+        this.products.push(item); // пушваме масив от string
         this._budget -= price;
         return `You have successfully bought ${item}!`;
     }
 
+    // Method
     recipes(recipe) {
         const productsList = recipe.productsList;
         const recipeName = recipe.recipeName;
@@ -38,21 +46,24 @@ class ChristmasDinner {
             }
         });
 
-        this.dishes.push({ recipeName, productsList });
+        // При преминаване на проверката следва добавянето в масива
+        this.dishes.push({ recipeName, productsList }); // пушваме масива, вкарваме ги в един обект и ги пушваме вътре
         return `${recipeName} has been successfully cooked!`;
     }
 
+    // Method
     inviteGuests(name, dish) {
         if (!this.dishes.values(dish)) {
             return "We do not have this dish";
         } else if (this.guests[name]) {
             throw new Error("This guest has already been invited");
         }
-
+        
         this.guests[name] = dish;
         return `You have successfully invited ${name}!`;
     }
 
+    // Method
     showAttendance() {
         let result = [];
         for (const name in this.guests) {
@@ -64,28 +75,35 @@ class ChristmasDinner {
     }
 }
 
-// let dinner = new ChristmasDinner(300);
-// console.log(dinner.shopping(['Salt', 1]));
-// console.log(dinner.shopping(['Beans', 3]));
-// console.log(dinner.shopping(['Cabbage', 4]));
-// console.log(dinner.shopping(['Rice', 2]));
-// console.log(dinner.shopping(['Savory', 1]));
-// console.log(dinner.shopping(['Peppers', 1]));
-// console.log(dinner.shopping(['Fruits', 40]));
-// console.log(dinner.shopping(['Honey', 10]));
-// console.log(dinner.recipes({
-//     recipeName: 'Oshav',
-//     productsList: ['Fruits', 'Honey']
-// }));
-// console.log(dinner.recipes({
-//     recipeName: 'Folded cabbage leaves filled with rice',
-//     productsList: ['Cabbage', 'Rice', 'Salt', 'Savory']
-// }));
-// console.log(dinner.recipes({
-//     recipeName: 'Peppers filled with beans',
-//     productsList: ['Beans', 'Peppers', 'Salt']
-// }));
-// console.log(dinner.inviteGuests('Ivan', 'Oshav'));
-// console.log(dinner.inviteGuests('Petar', 'Folded cabbage leaves filled with rice'));
-// console.log(dinner.inviteGuests('Georgi', 'Peppers filled with beans'));
-// console.log(dinner.showAttendance());
+// Example for check how works our code
+let dinner = new ChristmasDinner(300);
+console.log(dinner.shopping(['Salt', 1]));
+console.log(dinner.shopping(['Beans', 3]));
+console.log(dinner.shopping(['Cabbage', 4]));
+console.log(dinner.shopping(['Rice', 2]));
+console.log(dinner.shopping(['Savory', 1]));
+console.log(dinner.shopping(['Peppers', 1]));
+console.log(dinner.shopping(['Fruits', 40]));
+console.log(dinner.shopping(['Honey', 10]));
+console.log(dinner.recipes({
+    recipeName: 'Oshav',
+    productsList: ['Fruits', 'Honey']
+}));
+console.log(dinner.recipes({
+    recipeName: 'Folded cabbage leaves filled with rice',
+    productsList: ['Cabbage', 'Rice', 'Salt', 'Savory']
+}));
+console.log(dinner.recipes({
+    recipeName: 'Peppers filled with beans',
+    productsList: ['Beans', 'Peppers', 'Salt']
+}));
+console.log(dinner.inviteGuests('Ivan', 'Oshav'));
+console.log(dinner.inviteGuests('Petar', 'Folded cabbage leaves filled with rice'));
+console.log(dinner.inviteGuests('Georgi', 'Peppers filled with beans'));
+console.log(dinner.showAttendance());
+
+
+// class
+// constructor
+// this
+// forEach
