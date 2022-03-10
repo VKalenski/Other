6ba0c608bfd0b9222f6a20f66a206108b3560c71
VKalenski <vilislav.kalenski@elando.com>
2022-03-10 11:21:25 +0200

    Update

--- JS_Advanced/JS Advanced - Exams/Exam Prep 2 - 15.02.2021/03/tests.test.js
+++ JS_Advanced/JS Advanced - Exams/Exam Prep 2 - 15.02.2021/03/tests.test.js
@@ -2,47 +2,57 @@ const dealership = require('./03.Dealership');
 const expect = require('chai').expect;
 
 describe('test', () => {
-    it('works', () => {
-        expect(1).to.equal(1); // функцията it отново приема два параметъра: името на категорията и функция, която да се изпълни
-    }); // т.е. ако функцията () => хвърли грешка, mocha казва, че теста не е минал
-}); // за да хвърли грешка използваме expect, който идва от chai
+    describe('newCarCost', () => {
+        // model is suported -> price rediced by model value
+        it('returns discounted price', () => {
+            expect(dealership.newCarCost('Audi A4 B8', 30000)).to.equal(15000); // лявата част трябва да е равна на дясната, т.е. след to.equal
+        });
 
+        // model is not suported -> no discount
+        it('returns original price when model unsuported', () => {
+            expect(dealership.newCarCost(['a'], 1)).to.equal(1);
+        });
+    });
 
-// describe - синтаксис от Mocha. Приема два параметъра: първият е името на категорията (test), а вторият: функция - () => ...;
+    describe('carEquipment', () => {
+        // single element, single pick
+        it('single element, single pick', () => {
+            expect(dealership.carEquipment(['a'], [0])).to.deep.equal(['a']);
+        });
 
+        // test overloading - multiple elements, multiple picks
+        it('single element, single pick', () => {
+            expect(dealership.carEquipment(['a', 'b', 'c'], [0, 2])).to.deep.equal(['a', 'c']);
+        });
 
+        it('single element, single pick', () => {
+            expect(dealership.carEquipment(['a', 'b', 'c'], [1])).to.deep.equal(['b']);
+        });
+    });
 
+    describe('euroCategory', () => {
+        // category is below threshold - необходим ни е, за да разберем какво става, когато сме под границата
+        it('category is below threshold', () => {
+            expect(dealership.euroCategory(1)).to.equal('Your euro category is low, so there is no discount from the final price!');
+        });
 
+        // category is above threshold - необходим ни е, за да разберем какво става, когато преминем границата
+        it('category is above threshold', () => {
+            expect(dealership.euroCategory(5)).to.equal(`We have added 5% discount to the final price: 14250.`);
+        });
 
+        // category is at threshold (edge case - граничен случай) - необходим ни е, защото при промяна в основния код не се наблюдава покриване на гранчния случай
+        it('category is above threshold', () => {
+            expect(dealership.euroCategory(4)).to.equal(`We have added 5% discount to the final price: 14250.`);
+        });
+    });
+});
 
+// describe - синтаксис от Mocha. Приема два параметъра: първият е името на категорията (test), а вторият: функция - () => 
+// it - синтаксис от Mocha or Chai
+// expect - синтаксис от Mocha or Chai
 
-
-
-// const { assert } = require('chai');
-// const dealership = require('./03.Dealership.js');
-
-// describe('Dealer Ship Testing', () => {
-//     it('New CarCost', () => {
-//         assert.equal(dealership.newCarCost('Audi A4 B8', 100000), 85000);
-//         assert.equal(dealership.newCarCost('B', 1), 1);
-//     });
-
-//     it('Car Equipment', () => {
-//         assert.deepEqual(dealership.carEquipment(
-//             ['a', 'b', 'c', 'd'], []), []);
-
-//         assert.deepEqual(dealership.carEquipment(
-//             ['a', 'b', 'c', 'd'], [0, 2, 3]), ['a', 'c', 'd']);
-//     });
-
-//     it('Euro Category', () => {
-//         assert.equal(dealership.euroCategory(3),
-//             'Your euro category is low, so there is no discount from the final price!');
-
-//         assert.equal(dealership.euroCategory(4),
-//             `We have added 5% discount to the final price: 14250.`);
-
-//         assert.equal(dealership.euroCategory(5),
-//             `We have added 5% discount to the final price: 14250.`)
-//     });
-// });
+// За да напишем тестовете гледаме само спецификацията
+// Само ако нещо в спецификацията не е ясно, тогава гледаме кода
+// Важно е да хванем граничните случаи
+// Ако счупим основния код и тестовете минават - значи тестовете не са покрили част от сценариите
