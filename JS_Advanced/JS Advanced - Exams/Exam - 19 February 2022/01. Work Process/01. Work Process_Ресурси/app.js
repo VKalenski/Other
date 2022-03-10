0d3739c595dbce3795f26eafd23272cabc5e69c0
VKalenski <vilislav.kalenski@elando.com>
2022-03-10 18:18:10 +0200

    Update

--- JS_Advanced/JS Advanced - Exams/Exam - 19 February 2022/01. Work Process/01. Work Process_Ресурси/app.js
+++ JS_Advanced/JS Advanced - Exams/Exam - 19 February 2022/01. Work Process/01. Work Process_Ресурси/app.js
@@ -1,4 +1,97 @@
+window.addEventListener('load', solve);
+
 function solve() {
-    //TODO
+
+    // Logic for getting information from the hired form   
+    const addBtn = document.getElementById('add-worker');
+    const firstName = document.getElementById('fname');
+    const lastName = document.getElementById('lname');
+    const email = document.getElementById('email');
+    const dataOfBirth = document.getElementById('birth');
+    const position = document.getElementById('position');
+    const salary = document.getElementById('salary');
+    const newBody = document.getElementById('tbody');
+
+    // Attach event listeners to input form
+    addBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+
+    let fName = firstName.value;
+    let lName = lastName.value;
+    let eMail = email.value;
+    let dOfBirth = dataOfBirth.value;
+    let pos = position.value;
+    let sal = Number(salary.value);
+
+    firstName.value = '';
+    lastName.value = '';
+    email.value = '';
+    dataOfBirth.value = '';
+    position.value = '';
+    salary.value = '';
+    newBody.value = '';
+
+    let rowElement = document.createElement('tr'); // Създаване на цял нов ред
+    let firstNameCell = document.createElement('td'); // Създаване на колона в реда
+    let lastNameCell = document.createElement('td'); // Създаване на колона в реда
+    let emailCell = document.createElement('td'); // Създаване на колона в реда
+    let dataOfBirthCell = document.createElement('td'); // Създаване на колона в реда
+    let positionCell = document.createElement('td'); // Създаване на колона в реда
+    let salaryCell = document.createElement('td'); // Създаване на колона в реда  
+
+    firstNameCell.textContent = fName.value;
+    lastNameCell.textContent = lName.value;
+    emailCell.textContent = eMail.value;
+    dataOfBirthCell.textContent = dOfBirth.value;
+    positionCell.textContent = pos.value;
+    salaryCell.textContent = sal.value;
+
+    rowElement.appendChild(firstNameCell);
+    rowElement.appendChild(lastNameCell);
+    rowElement.appendChild(emailCell);
+    rowElement.appendChild(dataOfBirthCell);
+    rowElement.appendChild(positionCell);
+    rowElement.appendChild(salaryCell);
+
+    newBody.appendChild(rowElement);
+
+
+
+    //     const fName = firstName.value;
+    //     const lName = lastName.value;
+    //     const eMail = email.value;
+    //     const dOfBirth = dataOfBirth.value;
+    //     const pos = position.value;
+    //     const sal = salary.value;
+
+    //     firstName.value = ''; // по този начин изчистваме value -> правим го празен string
+    //     lastName.value = ''; // по този начин изчистваме value -> правим го празен string
+    //     email.value = ''; // по този начин изчистваме value -> правим го празен string
+    //     dataOfBirth.value = ''; // по този начин изчистваме value -> правим го празен string
+    //     position.value = ''; // по този начин изчистваме value -> правим го празен string
+    //     salary.value = ''; // по този начин изчистваме value -> правим го празен string
+
+    //     const element = e('submit', fName, lName, eMail, dOfBirth, pos, sal, 'tbl-content')
+    // }
+
+
+    // Logic for Edit button - remove person from list and salary change
+
+
+    //     // Logic for Fired button - remove person from list
+
+    // //     function e(type, content, className) {
+    // //         const result = document.createElement(type);
+    // //         result.textContent = content;
+    // //         if (className) {
+    // //             result.className = className;
+    // //         }
+
+    // //         return result;
+    // //     }
+
+
+
 }
-solve()
+// solve()
---
+++ JS_Advanced/JS Advanced - Exams/Prepare exam - xxx/js-advanced-exam-preparation-I/js-advanced-exam-preparation-I/01. Furniture Store/app.js
@@ -0,0 +1,94 @@
+window.addEventListener('load', solve);
+
+function solve() {
+    const addButtonElement = document.getElementById('add'); // референция към съответен DOM обект
+    const modelInputElement = document.getElementById('model'); // референция към съответен DOM обект
+    const yearInputElement = document.getElementById('year'); // референция към съответен DOM обект
+    const descriptionInputElement = document.getElementById('description'); // референция към съответен DOM обект
+    const priceInputElement = document.getElementById('price'); // референция към съответен DOM обект
+    const furnitureListElement = document.getElementById('furniture-list'); // референция към съответен DOM обект
+
+    addButtonElement.addEventListener('click', (e) => {
+        e.preventDefault(); // default-тното поведение е рефреш на страницата и с това ние спираме това действие
+
+        let model = modelInputElement.value; 
+        let description = descriptionInputElement.value;
+        let year = Number(yearInputElement.value); // parse-ваме към number
+        let price = Number(priceInputElement.value); // parse-ваме към number
+
+        modelInputElement.value = ''; // празен string
+        descriptionInputElement.value = ''; // празен string
+        yearInputElement.value = ''; // празен number
+        priceInputElement.value = ''; // празен number
+
+        // Ако if проверките не са изпълнени, няма да продължим изпълнението на кода
+        if (!model || !description) { // проверка в случай, че model and description ги няма
+            return;
+        }
+
+        if (year <= 0 || price <= 0) {
+            return;
+        }
+
+        let rowElement = document.createElement('tr'); // 
+        let modelCellElement = document.createElement('td'); // 
+        let priceCellElement = document.createElement('td'); // 
+        let actionsCellElement = document.createElement('td'); // 
+        let infoButtonElement = document.createElement('button'); // 
+        let buyButtonElement = document.createElement('button'); // 
+        let contentsRowElement = document.createElement('tr'); // 
+        let yearContentElement = document.createElement('td'); // 
+        let descriptionContentElement = document.createElement('td'); // 
+        let totalPriceElement = document.querySelector('.total-price'); //
+        
+        modelCellElement.textContent = model;
+        priceCellElement.textContent = price.toFixed(2);
+
+        infoButtonElement.textContent = 'More Info';
+        infoButtonElement.classList.add('moreBtn');
+        infoButtonElement.addEventListener('click', (e) => {
+            if (e.currentTarget.textContent == 'More Info') {
+                contentsRowElement.style.display = 'contents';
+                e.currentTarget.textContent = 'Less Info';
+            } else {
+                contentsRowElement.style.display = 'none';
+                e.currentTarget.textContent = 'More Info';
+            }
+        });
+
+        buyButtonElement.textContent = 'Buy it';
+        buyButtonElement.classList.add('buyBtn');
+        buyButtonElement.addEventListener('click', (e) => {
+            let currentTotalPrice = Number(totalPriceElement.textContent);
+            let totalPrice = currentTotalPrice + price;
+            totalPriceElement.textContent = totalPrice.toFixed(2);
+            
+            rowElement.remove();
+            contentsRowElement.remove();
+        });
+
+        actionsCellElement.appendChild(infoButtonElement);
+        actionsCellElement.appendChild(buyButtonElement);
+
+        rowElement.classList.add('info');
+
+        rowElement.appendChild(modelCellElement);
+        rowElement.appendChild(priceCellElement);
+        rowElement.appendChild(actionsCellElement);
+
+        yearContentElement.textContent = `Year: ${year}`;
+        descriptionContentElement.setAttribute('colspan', 3);
+        descriptionContentElement.textContent = `Description: ${description}`;
+
+        contentsRowElement.classList.add('hide');
+        contentsRowElement.style.display = 'none';
+
+        contentsRowElement.appendChild(yearContentElement);
+        contentsRowElement.appendChild(descriptionContentElement);
+
+        furnitureListElement.appendChild(rowElement);
+        furnitureListElement.appendChild(contentsRowElement);
+    });
+}
+
+// const
