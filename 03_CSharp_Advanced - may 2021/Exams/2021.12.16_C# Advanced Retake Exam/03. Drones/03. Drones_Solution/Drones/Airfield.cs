15f3ceb7d4c3374e806f6bf7302c634f9f7ba25e
VKalenski <vilislav.kalenski@elando.com>
2022-04-12 10:08:22 +0300

    Update files

---
+++ 03_CSharp_Advanced - may 2021/Exams/2021.12.16_C# Advanced Retake Exam/03. Drones/03. Drones_Solution/Drones/Airfield.cs
@@ -0,0 +1,17 @@
+﻿namespace Drones
+{
+    public class Airfield
+    {
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.LandingStrip = landingStrip;
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public double LandingStrip { get; set; }
+
+
+    }
+}
---
+++ 03_CSharp_Advanced - may 2021/Exams/2021.12.16_C# Advanced Retake Exam/03. Drones/03. Drones_Solution/Drones/Drone.cs
@@ -0,0 +1,23 @@
+﻿namespace Drones
+{
+    public class Drone
+    {
+        public Drone(string name, string brand, int range)
+        {
+            this.Name = name;
+            this.Brand = brand;
+            this.Range = range;
+        }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Range { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drone: {Name} \n\r" +
+                   $"Manufactured by: { Brand} \n\r" +
+                   $"Range: { Range} \n\r" +
+                   $"kilometers";
+        }
+    }
+}
